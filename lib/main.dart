83322dca59942d952488bd4dfe59804534b8dbea
Amaury <primal.coding@gmail.com>
2019-10-29 17:40:45 +0000

    Logo displayed with a sizeable white border - other previous simpler coding options left in comments (with no possibility to change the size of the border due to the Logo being too small inside the whole picture file)

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: ChangeNotifierProvider<Counter>(
         builder: (_) => Counter(0),
         child: Consumer<Counter>(
-          builder: (context, counter, _){
+          builder: (context, counter, _) {
             return MyHomePage(title: 'Flutter Demo Home Page');
           },
         ),
@@ -35,17 +34,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     final _counter = Provider.of<Counter>(context);
 
     return Scaffold(
-      
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              width: 300.0,//MediaQuery.of(context).size.width / 3,     // 1/3 of screen width
+              height: 300.0, //MediaQuery.of(context).size.width / 3,
+              // decoration: BoxDecoration(
+              //   border: Border.all(width:10.0),
+              //   color: Colors.black, // todo: Change To White
+              //   shape: BoxShape.circle,
+              //   image: DecorationImage(
+              //     fit: BoxFit.cover, // optional in our case
+              //     // the original image has to be modified in order to get a thiner border
+              //     image: AssetImage('assets/officiallogo.png')
+              //   )
+              // ),
+              child: Material(
+                shape: CircleBorder(
+                  side: BorderSide(
+                    //color: new ThemeData().dialogBackgroundColor,
+                    color: Colors.black,  // todo : to change to the backgroung color 
+                    width: 17.0           // 17 for 300 // 5 or 6 for 100
+                  )
+                ),
+                color: Colors.white,
+                child: Image.asset('assets/officiallogo.png'),
+              ),
+              // child: CircleAvatar(
+              //   backgroundColor: Colors.black,
+              //   backgroundImage: AssetImage('assets/officiallogo.png'),
+              // ),
+            ),
             Text(
               'You have pushed the button this many times:',
             ),
@@ -56,39 +82,41 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: () => Provider.of<Counter>(context, listen: false).increment(),
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () => Provider.of<Counter>(context, listen: false).decrement(),
-              tooltip: 'Increment',
-              child: Icon(Icons.remove),
-            )
-          ],
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () =>
+              Provider.of<Counter>(context, listen: false).increment(),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () =>
+              Provider.of<Counter>(context, listen: false).decrement(),
+            tooltip: 'Increment',
+            child: Icon(Icons.remove),
+          )
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
 
-class Counter with ChangeNotifier{
+class Counter with ChangeNotifier {
   int _counter;
 
   Counter(this._counter);
   get counter => this._counter;
   set counter(int counter) => this._counter = counter;
 
-  void increment(){
+  void increment() {
     _counter++;
     notifyListeners();
   }
 
-  void decrement(){
+  void decrement() {
     _counter--;
     notifyListeners();
   }
